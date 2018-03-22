9c13543acab67360b1d1e053351a75fbf678e611
Guest User <Guest@Epicodus-4D.local>
2018-03-22 10:46:30 -0700

    Add styling, functionality

--- src/main.js
+++ src/main.js
@@ -3,22 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import '../.env';
 
-var MoviePick = require('./moviepick.js').moviePick;
+import {MoviePick} from './moviepick.js';
 
-var displayData = function(response) {
-  response.results.forEach(function(result) {
-    $('.showList').append('<li>' + result.title + '</li>');
+const displayData = response => {
+  response.results.forEach(result => {
+    $('.showList').append(`<li>${result.title}</li>`);
   });
-}
+};
 
-$(document).ready(function() {
-  var moviePick = new MoviePick();
+$(document).ready(() => {
+  const moviePick = new MoviePick();
 
-  $('#yearClick').click(function (event) {
+  $('#yearClick').click(event => {
     $('.showList').empty()
     event.preventDefault();
-    console.log(userInput);
-    var userInput = $('#year').val()
+    let userInput = $('#year').val()
     $('#showInfo').text(`Here are some of the most popular movies for ` + userInput + `.`)
     MoviePick.prototype.getData(userInput, displayData)
   });
--- src/moviepick.js
+++ src/moviepick.js
@@ -1,42 +1,14 @@
 import $ from 'jquery';
-var dotenv = require('dotenv')
+import dotenv from 'dotenv';
 dotenv.load()
 
-var MoviePick = function() {
-
+export class MoviePick {
+  getData(userInput, displayData) {
+  const tmdbKey = process.env.API_KEY
+    $.get(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&primary_release_year=${userInput}&vote_count.gte=100`)
+    .then(response => {
+      displayData(response);
+    })
+    .fail(() => 'something went wronggggg')
+  }
 }
-
-MoviePick.prototype.getData = function(userInput, displayData) {
-const tmdbKey = process.env.API_KEY
-  $.get(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&primary_release_year=${userInput}&vote_count.gte=1000`)
-  .then(function(response) {
-    displayData(response);
-    console.log("HERE IS USER ENTERED YEAR", userInput);
-  })
-  .fail(function() {
-    return 'something went wronggggg'
-  })
-}
-
-exports.moviePick = MoviePick;
-
-
-// export class MoviePick {
-//
-//   constructor(userInput) {
-//     this.userInput = userInput;
-//   }
-//
-//   getData() {
-//     const tmdbKey = process.env.API_KEY
-//     $.get(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&primary_release_year=${userInput}&vote_count.gte=1000`)
-//     .then(function(response) {
-//       displayData(response);
-//       console.log("HERE IS USER ENTERED YEAR", userInput);
-//     });
-//   }
-//   .fail() {
-//       return 'something went wronggggg'
-//     });
-//   }
-// }
